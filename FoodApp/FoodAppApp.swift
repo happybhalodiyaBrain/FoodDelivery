dbb01b0593c6ebc22be2bb1269e4de8f602109f1
happy.bhalodiya <happy.bhalodiya@brainvire.com>
2024-12-30 17:26:34 +0530

    Initial Commit

---
+++ FoodApp/FoodAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  FoodAppApp.swift
+//  FoodApp
+//
+//  Created by Happy  Bhalodiya on 30/12/24.
+//
+
+import SwiftUI
+
+@main
+struct FoodAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
